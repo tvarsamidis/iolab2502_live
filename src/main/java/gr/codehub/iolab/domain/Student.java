b9feb17027d508f6c45436785be7409f43c74955
Thomas the Great <no email yet>
2025-02-03 12:17:55 +0200

    Add first version of Student

---
+++ src/main/java/gr/codehub/iolab/domain/Student.java
@@ -0,0 +1,20 @@
+package gr.codehub.iolab.domain;
+
+public class Student {
+    private String id;
+    private String name;
+
+    public Student(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
--- src/main/java/gr/codehub/iolab/interfaces/HelloWorld.java
+++ src/main/java/gr/codehub/iolab/interfaces/HelloWorld.java
@@ -1,7 +1,7 @@
-package gr.codehub.iolab;
+package gr.codehub.iolab.interfaces;
 
 import ch.qos.logback.classic.Level;
-import org.slf4j.Logger;
+import gr.codehub.iolab.domain.Student;
 import org.slf4j.LoggerFactory;
 
 public class HelloWorld {
@@ -12,10 +12,13 @@ public class HelloWorld {
             (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(HelloWorld.class);
 
     public static void main(String[] args) {
-        logger.setLevel(Level.WARN);
         logger.info("This is the logger speaking!");
         logger.warn("This is a warning!");
         logger.error("The code reached an error case");
         System.out.println("Hello world!");
+
+        Student s1 = new Student("ABCD", "Thomas Student2");
+        logger.info("The first student {} has id={}", s1.getName(), s1.getId());
+
     }
 }
